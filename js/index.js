874bd888674b86311d80478be9b11f33d4be3507
Grupo Hernández Alba <marco@grupohernandezalba.com>
2024-09-17 13:59:18 +0000

    proyecto inicial

---
+++ js/index.js
@@ -0,0 +1,16 @@
+console.log("Hola Mundo");
+
+let sumar = 10 + 25;
+
+console.log(sumar);
+
+let a = 40;
+let b = 20;
+
+//Se multiplican las variables a por b 
+const multiplicar = (a,b) => a * b {
+    if (typeof a!== 'number') ||
+    typeof b !== 'number'
+}
+
+
